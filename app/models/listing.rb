5df6404ef9775bd059f6f3f164ddea70da3ea028
Justin Cook <juscook10@gmail.com>
2014-10-23 07:34:44 -0500

    configured localhost / dropbox seperation

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,10 @@
 class Listing < ActiveRecord::Base
-	has_attached_file :image, :styles => { :large => "570x>", :page => "270x180>", :medium => "200x>", :thumb => "116x77>" }, :default_url => "no_image_available.jpg", :storage => :dropbox,
-    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	if Rails.env.development?
+		has_attached_file :image, :styles => { :large => "570x>", :page => "270x180>", :medium => "200x>", :thumb => "116x77>" }, :default_url => "no_image_available.jpg"
+	else
+		has_attached_file :image, :styles => { :large => "570x>", :page => "270x180>", :medium => "200x>", :thumb => "116x77>" }, :default_url => "no_image_available.jpg", :storage => :dropbox,
+    					  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+    					  :path => ":style/:id_:filename"
+    	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+    end
 end
